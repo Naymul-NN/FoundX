23639a5830df571e13348ef8d0277cee66b40b43
Naymul-NN <naymulislam108@gmail.com>
2024-10-17 14:43:15 +0600

    create the route group

---
+++ src/app/(withCommonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/src/components/navbar";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <div className="relative flex flex-col h-screen">
+        <Navbar />
+        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1> This is dashboard page .. </h1>
+    </div>
+  );
+}
